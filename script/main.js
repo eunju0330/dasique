e349e6623c9133fba65c3776fe9b7e33d584bae1
eunju <echoe7894@gmail.com>
2025-07-31 19:21:14 +0900

    푸터 전

--- script/main.js
+++ script/main.js
@@ -1,8 +1,16 @@
-const shortsSwiper = new Swiper('#shorts_swiper', {
-    // …기존 설정…
-    scrollbar: {
-        el:'.swiper-scrollbar',
-        draggable: true,
-        hide:false,
-    },
-});
+const btns = document.querySelectorAll('.store_wrap .tab_btn');
+const contents = document.querySelectorAll('.store_wrap .tab_content');
+const imgs = document.querySelectorAll('.store_wrap .left_image');
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', function() {
+        for (let j = 0; j < btns.length; j++) {
+            btns[j].classList.remove('active');
+            contents[j].classList.remove('active');
+            imgs[j].classList.remove('active');
+        }
+        btns[i].classList.add('active');
+        contents[i].classList.add('active');
+        imgs[i].classList.add('active');
+    });
+}
