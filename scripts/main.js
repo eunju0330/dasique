29aa1af36b7829c21f7f8f179a8e4d9ab2272db6
hyobin9824 <hyobin9824@gmail.com>
2025-08-05 19:19:25 +0900

    list 남음

---
+++ scripts/main.js
@@ -0,0 +1,52 @@
+/* list */
+const productSwiper = new Swiper ('#listswiper', {
+    direction:'horizontal',
+    slidesPerView:4,
+    spaceBetween:48,
+    slidesPerGroup:1,
+    scrollbar:{
+        el:'.swiper-scrollbar',
+        draggable:true,
+    }
+});
+
+/* collection */
+const collectionSwiper = new Swiper('#collectionSwiper', {
+    slidesPerView:1,
+    spaceBetween:32,
+    loop:false,
+    navigation:{
+        nextEl:'.swiper-button-next',
+        prevEl:'.swiper-button-prev',
+    },
+});
+
+/* store */
+const btns = document.querySelectorAll('.store_wrap .tab_btn');
+const contents = document.querySelectorAll('.store_wrap .tab_content');
+const imgs = document.querySelectorAll('.store_wrap .left_image');
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', function() {
+        for (let j = 0; j < btns.length; j++) {
+            btns[j].classList.remove('active');
+            contents[j].classList.remove('active');
+            imgs[j].classList.remove('active');
+        }
+        btns[i].classList.add('active');
+        contents[i].classList.add('active');
+        imgs[i].classList.add('active');
+    });
+}
+
+/* shorts */
+const shortsSwiper = new Swiper('#shorts_swiper', {
+    slidesPerView:5,
+    spaceBetween:35,
+    slidesPerGroup:1,
+    loop:false,
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+    },
+});
